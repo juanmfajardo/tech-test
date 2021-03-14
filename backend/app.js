c73d20a1e3176ee1c6cb174ef61faa5c6167b31c
Juanma <juanma@strivelabs.io>
2021-03-14 11:53:04 +0100

    Feat | Clean app server file

--- backend/app.js
+++ backend/app.js
@@ -1,100 +1,20 @@
 import express from 'express';
-import http from 'http';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import Debug from 'debug';
 
-import contacts from './routes/contacts.js';
+import config from './src/config/index.js';
+import loaders from './src/loaders/index.js';
 
-const app = express();
+const startServer = async () => {
+    const app = express();
+    await loaders(app);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+    app.listen(config.PORT, () => { 
+        console.log(`Backend running on port ${config.PORT} 🚀`);
+    })
 
-app.use('/contacts', contacts);
-
-var debug = Debug('backend:server');
-
-/**
- * Get port from environment and store in Express.
- */
-
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
+    app.on('error', (err) => {
+        console.log(err);
+        process.exit(1);
+    });
 }
 
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
-
+startServer();
