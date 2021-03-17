367488d4f7d242d341601bdbc654c3507c1af590
Juanma <juanma@strivelabs.io>
2021-03-17 03:02:29 +0100

    [F] Feat | Update App with ContactsTable component. Remove initial template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import ContactsTable from './Components/ContactsTable';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
+const App = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col>
+            <ContactsTable contacts={contacts} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
