439949204e4dd5294f0d517e4757b0573322d425
Juanma <juanma@strivelabs.io>
2021-03-17 03:05:20 +0100

    Fix | Eslint warnings

--- backend/src/api/controllers/contacts.js
+++ backend/src/api/controllers/contacts.js
@@ -1,7 +1,9 @@
 import Contact from '../../models/contact.js';
 
 export const addContact = async (req, res) => {
-    const { firstName, lastName, email, phoneNumber } = req.body;
+    const {
+        firstName, lastName, email, phoneNumber,
+    } = req.body;
 
     const contactFields = {
         firstName,
@@ -14,39 +16,42 @@ export const addContact = async (req, res) => {
         {
             ...contactFields,
             updateHistory: contactFields,
-        }
+        },
     ).save();
-    
-    res.json({ message: 'Contact successfully added', contact }).status(200)
+
+    res.json({ message: 'Contact successfully added', contact }).status(200);
 };
 
 export const deleteContact = async (req, res) => {
     const { id } = req.params;
 
-    const contact = await Contact.findByIdAndUpdate(id, { deleted: true });
+    await Contact.findByIdAndUpdate(id, { deleted: true });
 
-    res.json({ message: 'Contact successfully deleted' }).status(200)
+    res.json({ message: 'Contact successfully deleted' }).status(200);
 };
 
 export const updateContact = async (req, res) => {
-    const { firstName, lastName, email, phoneNumber } = req.body;
+    const {
+        firstName, lastName, email, phoneNumber,
+    } = req.body;
     const { id } = req.params;
 
-    const contactsFieldsToUpdate = { 
+    const contactsFieldsToUpdate = {
         firstName,
         lastName,
         email,
         phoneNumber,
     };
 
-    Object.keys(contactsFieldsToUpdate).forEach(key => contactsFieldsToUpdate[key] === undefined && delete contactsFieldsToUpdate[key]);
+    Object.keys(contactsFieldsToUpdate).forEach((key) => (
+        contactsFieldsToUpdate[key] === undefined && delete contactsFieldsToUpdate[key]));
 
-    await Contact.findByIdAndUpdate(id, { 
+    await Contact.findByIdAndUpdate(id, {
         $set: contactsFieldsToUpdate,
         $push: { updateHistory: contactsFieldsToUpdate },
     }, { new: true });
-    
-    res.json({ message: 'Contact successfully updated', updatedFields: contactsFieldsToUpdate }).status(200)
+
+    res.json({ message: 'Contact successfully updated', updatedFields: contactsFieldsToUpdate }).status(200);
 };
 
 export const getContact = async (req, res) => {
@@ -54,11 +59,11 @@ export const getContact = async (req, res) => {
 
     const contact = await Contact.findById(id);
 
-    res.json({ message: 'Contact successfully obtained', contact }).status(200)
+    res.json({ message: 'Contact successfully obtained', contact }).status(200);
 };
 
 export const listContacts = async (req, res) => {
     const contacts = await Contact.find({ deleted: false });
 
-    res.json({ message: 'Contacts successfully obtained', contacts }).status(200)
+    res.json({ message: 'Contacts successfully obtained', contacts }).status(200);
 };
