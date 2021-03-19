472e7c410203c7e3a149acf3aeacdb395f7f3008
Juanma <juanma@strivelabs.io>
2021-03-19 03:52:00 +0100

    Feat | Add validators for the different endpoints

---
+++ backend/src/api/middlewares/validators.js
@@ -0,0 +1,68 @@
+import { body, param, validationResult } from 'express-validator';
+
+import { Contact } from '../../models/contact.js';
+
+const emailInUse = async (value) => {
+    const contact = Contact.findOne({ email: value });
+    if (contact) throw new Error('email already in use');
+    return true;
+};
+
+export const emptyFieldsValidation = () => [
+    body('firstName')
+        .notEmpty()
+        .withMessage('firstName can not be empty!'),
+    body('lastName')
+        .notEmpty()
+        .withMessage('lastName can not be empty!'),
+    body('phoneNumber')
+        .notEmpty()
+        .withMessage('phoneNumber can not be empty!'),
+    body('email')
+        .notEmpty()
+        .withMessage('email can not be empty!'),
+];
+
+export const validFieldsValidation = () => [
+    body('phoneNumber')
+        .isMobilePhone()
+        .withMessage('phoneNumber is not a valid Phone Number!'),
+    body('email')
+        .toLowerCase()
+        .isEmail()
+        .withMessage('email is not a valid Email!')
+        .custom(emailInUse),
+];
+
+export const createValidation = () => [...emptyFieldsValidation(), ...validFieldsValidation()];
+
+export const emailValidation = () => [
+    param('email')
+        .trim()
+        .toLowerCase()
+        .notEmpty()
+        .withMessage('email can not be empty!')
+        .bail()
+        .isEmail()
+        .withMessage('email is not a valid Email!'),
+];
+
+export const idValidation = () => [
+    param('id')
+        .trim()
+        .notEmpty()
+        .withMessage('id can not be empty!')
+        .bail()
+        .isMongoId()
+        .withMessage('id is not a valid Id!'),
+];
+
+export const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) return next();
+
+    const extractedErrors = [];
+    errors.array().map((err) => extractedErrors.push({ [err.param]: err.msg }));
+
+    return res.status(422).json({ errors: extractedErrors });
+};
--- backend/src/api/routes/contacts.js
+++ backend/src/api/routes/contacts.js
@@ -1,22 +1,21 @@
 import { Router } from 'express';
 import {
-    addContact,
-    getContact,
-    listContacts,
-    deleteContact,
-    updateContact,
+    addContact, getContact, listContacts, deleteContact, updateContact, checkExistsEmail,
 } from '../controllers/contacts.js';
+import {
+    createValidation, emailValidation, validate, validFieldsValidation, idValidation,
+} from '../middlewares/validators.js';
 
 const contactRoutes = Router();
 
 const contacts = (router) => {
     router.use('/contacts', contactRoutes);
 
-    contactRoutes.post('/', addContact);
+    contactRoutes.post('/', createValidation(), validate, addContact);
     contactRoutes.get('/', listContacts);
-    contactRoutes.get('/:id', getContact);
-    contactRoutes.delete('/:id', deleteContact);
-    contactRoutes.patch('/:id', updateContact);
+    contactRoutes.get('/:id', idValidation(), validate, getContact);
+    contactRoutes.delete('/:id', idValidation(), validate, deleteContact);
+    contactRoutes.patch('/:id', idValidation(), validFieldsValidation(), validate, updateContact);
 };
 
 export default contacts;
