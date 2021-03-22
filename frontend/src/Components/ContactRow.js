61a421aedd54321e0e031b024427d9be08936fa1
Juanma <juanma@strivelabs.io>
2021-03-22 05:18:53 +0100

    [F] Feat | Pass contact to ContactOptions component. Rename import. Remove key

--- frontend/src/Components/ContactRow.js
+++ frontend/src/Components/ContactRow.js
@@ -1,14 +1,14 @@
-import Actions from './ContactOptions';
+import ContactOptions from './ContactOptions';
 
 const ContactRow = ({ contact }) => (
-  <tr key={contact.id}>
+  <tr>
     <td>{contact.id}</td>
     <td>{contact.firstName}</td>
     <td>{contact.lastName}</td>
     <td>{contact.email}</td>
     <td>{contact.phoneNumber}</td>
     <td>
-      <Actions />
+      <ContactOptions contact={contact} />
     </td>
   </tr>
 );
