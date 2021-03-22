c783686a6930146cdaceaa0af51b027d34a85c17
Juanma <juanma@strivelabs.io>
2021-03-22 05:22:43 +0100

    [F] Feat | Update main App component. Add different states, handlers and components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Alert from 'react-bootstrap/Alert';
 
 import ContactsTable from './Components/ContactsTable';
+import FloatingButton from './Components/FloatingButton';
+import FormModal from './Components/FormModal';
+import HistoryModal from './Components/HistoryModal';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+import {
+  getContacts, getContact, removeContact, updateContact, createContact,
+} from './Utils/helper';
+
+import DataContext from './Utils/context';
+
 const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [contact, setContact] = useState(null);
+  const [error, setError] = useState('');
+  const [showFormModal, setShowFormModal] = useState(false);
+  const [showHistoryModal, setShowHistoryModal] = useState(false);
+
+  const getInitialData = async () => {
+    const c = await getContacts();
+    if (Array.isArray(c)) {
+      setContacts(c);
+    } else {
+      setError(c);
+    }
+  };
+
+  const handleUpdateContact = (contactToUpdate) => {
+    setShowFormModal(true);
+    setContact(contactToUpdate);
+  };
+
+  const handleRemoveContact = async (contactId) => {
+    const removedContact = await removeContact(contactId);
+    if (removedContact) {
+      setContacts(contacts.filter((c) => c.id !== contactId));
+    }
+  };
+
+  const handleSubmitForm = async (fields) => {
+    const isUpdate = !!fields?.id;
+
+    if (isUpdate) {
+      const updatedfields = await updateContact(fields);
+      if (updatedfields) {
+        const index = contacts.findIndex((c) => c.id === fields.id);
+        const contactsCopy = [...contacts];
+        Object.keys(updatedfields).forEach((key) => {
+          contactsCopy[index][key] = updatedfields[key];
+        });
+        setContacts(contactsCopy);
+        setShowFormModal(false);
+      }
+    } else {
+      const newContact = await createContact(fields);
+      setContacts([...contacts, newContact]);
+      setShowFormModal(false);
+    }
+  };
+
+  const handleShowUpdateHistory = async (contactId) => {
+    setShowHistoryModal(true);
+    const c = await getContact(contactId);
+    setContact(c);
+  };
+
+  const handleCloseModal = (modal) => {
+    if (modal === 'history') setShowHistoryModal(false);
+    else setShowFormModal(false);
+    setContact(null);
+  };
+
+  useEffect(() => {
+    getInitialData();
+  }, []);
 
   return (
-    <div className="App">
-      <Container>
-        <Row>
-          <Col>
-            <ContactsTable contacts={contacts} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <DataContext.Provider
+      value={{
+        handleUpdateContact,
+        handleRemoveContact,
+        handleShowUpdateHistory,
+      }}
+    >
+      <div className="App">
+        {!!error && (
+        <Alert variant="danger" onClose={() => setError('')} dismissible>
+          {error}
+        </Alert>
+        )}
+        <FormModal
+          visible={showFormModal}
+          handleClose={() => handleCloseModal('form')}
+          contact={contact}
+          handleSubmitForm={handleSubmitForm}
+        />
+        <HistoryModal
+          visible={showHistoryModal}
+          handleClose={() => handleCloseModal('history')}
+          contact={contact}
+        />
+        <Container>
+          <Row>
+            <Col>
+              <ContactsTable
+                contacts={contacts}
+              />
+            </Col>
+          </Row>
+          <FloatingButton handleClick={() => setShowFormModal(true)} />
+        </Container>
+      </div>
+    </DataContext.Provider>
   );
 };
 
