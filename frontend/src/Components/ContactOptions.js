144d006e427a5bb6f4ad16b25c92f5ed62692fe3
Juanma <juanma@strivelabs.io>
2021-03-22 05:16:53 +0100

    [F] Feat | Add buttons component to display update history, edit and remove contacts

--- frontend/src/Components/ContactOptions.js
+++ frontend/src/Components/ContactOptions.js
@@ -1,12 +1,23 @@
+import { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { BsPencil, BsTrash, BsEye } from 'react-icons/bs';
 
-const contactOptions = () => (
-  <div>
-    <Button variant="primary" size="sm" className="mr-2"><BsEye /></Button>
-    <Button variant="warning" size="sm" className="mr-2"><BsPencil /></Button>
-    <Button variant="danger" size="sm"><BsTrash /></Button>
-  </div>
-);
+import DataContext from '../Utils/context';
+
+const contactOptions = ({ contact }) => {
+  const {
+    handleUpdateContact,
+    handleShowUpdateHistory,
+    handleRemoveContact,
+  } = useContext(DataContext);
+  return (
+    <ButtonGroup>
+      <Button variant="primary" size="sm" onClick={() => handleShowUpdateHistory(contact.id)}><BsEye /></Button>
+      <Button variant="warning" size="sm" onClick={() => handleUpdateContact(contact)}><BsPencil /></Button>
+      <Button variant="danger" size="sm" onClick={() => handleRemoveContact(contact.id)}><BsTrash /></Button>
+    </ButtonGroup>
+  );
+};
 
 export default contactOptions;
