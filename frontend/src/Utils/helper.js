327391e4e6294ec7dd7bab2c84eed13061159e5b
Juanma <juanma@strivelabs.io>
2021-03-22 05:11:11 +0100

    [F] Feat | Add functions to call backend endpoints

---
+++ frontend/src/Utils/helper.js
@@ -0,0 +1,95 @@
+import CONSTANTS from './constants';
+
+export const getContacts = async () => {
+  try {
+    const contacts = await fetch(`${CONSTANTS.API_HOST}${CONSTANTS.CONTACTS}`);
+    const json = await contacts.json();
+    const response = json.contacts || [];
+
+    return response;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+export const getContact = async (id) => {
+  try {
+    const contact = await fetch(`${CONSTANTS.API_HOST}${CONSTANTS.CONTACTS}/${id}`);
+    const json = await contact.json();
+    const response = json.contact || null;
+
+    return response;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+export const removeContact = async (id) => {
+  try {
+    const contact = await fetch(`${CONSTANTS.API_HOST}${CONSTANTS.CONTACTS}/${id}`, {
+      method: 'DELETE',
+    });
+    const json = await contact.json();
+    const response = json.contact || null;
+    console.log(response);
+    return true;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+export const updateContact = async (fields) => {
+  try {
+    const {
+      firstName, lastName, phoneNumber, email, id,
+    } = fields;
+
+    const updateContactRes = await fetch(`${CONSTANTS.API_HOST}${CONSTANTS.CONTACTS}/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        phoneNumber,
+        email,
+      }),
+    });
+
+    const json = await updateContactRes.json();
+    const response = json.updatedFields || null;
+    return response;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+export const createContact = async (fields) => {
+  try {
+    const {
+      firstName, lastName, phoneNumber, email,
+    } = fields;
+
+    const updateContactRes = await fetch(`${CONSTANTS.API_HOST}${CONSTANTS.CONTACTS}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        phoneNumber,
+        email,
+      }),
+    });
+
+    const json = await updateContactRes.json();
+    const response = json.contact || null;
+    return response;
+  } catch (error) {
+    return error.message;
+  }
+};
