70e96688fe8dd8739b6bbb3e527cd6fd615ff40f
Juanma <juanma@strivelabs.io>
2021-03-23 23:49:00 +0100

    Feat | Improve error handling for mongoose

--- backend/src/loaders/mongoose.js
+++ backend/src/loaders/mongoose.js
@@ -3,15 +3,20 @@ import mongoose from 'mongoose';
 import config from '../config/index.js';
 
 const mongooseLoader = async () => {
-    await mongoose.connect(config.MONGO_URL, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-    }).catch(console.log);
+    try {
+        await mongoose.connect(config.MONGO_URL, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+        });
 
-    mongoose.set('debug', true);
-    mongoose.set('useFindAndModify', false);
-    mongoose.set('toJSON', { virtuals: true });
+        mongoose.set('debug', true);
+        mongoose.set('useFindAndModify', false);
+        mongoose.set('toJSON', { virtuals: true });
+    } catch (error) {
+        console.log(error.message);
+        process.exit(1);
+    }
 };
 
 export default mongooseLoader;
