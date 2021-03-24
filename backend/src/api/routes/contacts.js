4cdb6f55a98a2819ead6d7384375c19390627756
Juanma <juanma@strivelabs.io>
2021-03-24 02:21:10 +0100

    Feat | User async handler in contact routes

--- backend/src/api/routes/contacts.js
+++ backend/src/api/routes/contacts.js
@@ -6,18 +6,20 @@ import {
     contactFieldsValidation, emailValidation, validate, idValidation,
 } from '../middlewares/validators.js';
 
+import asyncHandler from '../../utils/index.js';
+
 const contactRoutes = Router();
 
 const contacts = (router) => {
     router.use('/contacts', contactRoutes);
 
-    contactRoutes.get('/verify/:email?', emailValidation(), validate, checkExistsEmail);
+    contactRoutes.get('/verify/:email?', emailValidation(), validate, asyncHandler(checkExistsEmail));
 
-    contactRoutes.post('/', contactFieldsValidation(), validate, addContact);
-    contactRoutes.get('/', getContacts);
-    contactRoutes.get('/:id', idValidation(), validate, getContact);
-    contactRoutes.delete('/:id', idValidation(), validate, deleteContact);
-    contactRoutes.patch('/:id', idValidation(), validate, updateContact);
+    contactRoutes.post('/', contactFieldsValidation(), validate, asyncHandler(addContact));
+    contactRoutes.get('/', asyncHandler(getContacts));
+    contactRoutes.get('/:id', idValidation(), validate, asyncHandler(getContact));
+    contactRoutes.delete('/:id', idValidation(), validate, asyncHandler(deleteContact));
+    contactRoutes.patch('/:id', idValidation(), validate, asyncHandler(updateContact));
 };
 
 export default contacts;
