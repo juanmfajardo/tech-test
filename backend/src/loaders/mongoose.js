83f17d923d6cdcedb1eb37f5c6ea707c7bff9eba
Juanma <juanma@strivelabs.io>
2021-03-25 02:03:55 +0100

    Feat | Use isTestEnvironment function

--- backend/src/loaders/mongoose.js
+++ backend/src/loaders/mongoose.js
@@ -1,16 +1,18 @@
 import mongoose from 'mongoose';
 
 import config from '../config/index.js';
+import { isTestEnvironment } from '../utils/index.js';
 
 const mongooseLoader = async () => {
     try {
-        await mongoose.connect(`${config.MONGO_URL}${config.MONGO_DB}`, {
+        const mongoDb = isTestEnvironment() ? config.MONGO_TEST_DB : config.MONGO_TEST_DB;
+        await mongoose.connect(`${config.MONGO_URL}${mongoDb}`, {
             useNewUrlParser: true,
             useCreateIndex: true,
             useUnifiedTopology: true,
         });
 
-        mongoose.set('debug', true);
+        mongoose.set('debug', !isTestEnvironment());
         mongoose.set('useFindAndModify', false);
         mongoose.set('toJSON', { virtuals: true });
     } catch (error) {
