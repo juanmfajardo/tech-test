ad551d3219971045355f54f0a948226bbf692f0c
Juanma <juanma@strivelabs.io>
2021-03-25 02:03:30 +0100

    Feat | Add additional env vars

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -2,4 +2,6 @@ export default {
     PORT: process.env.PORT || 2000,
     MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/',
     MONGO_DB: process.env.MONGO_DB || 'tech_test',
+    MONGO_TEST_DB: process.env.MONGO_TEST_DB || 'test_tech_test',
+    NODE_ENV: process.env.NODE_ENV || 'dev',
 };
